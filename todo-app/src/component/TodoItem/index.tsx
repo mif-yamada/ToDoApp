c4168ec9e09f60e459723bcf4bb13812e21498b7
mosa <mosa@mosanoMacBook-Pro.local>
2021-11-19 13:35:30 +0900

    wip add,del終了 edit修正

---
+++ todo-app/src/component/TodoItem/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export interface TodoItemProps {
+  id: string;
+  task: string;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const TodoItemStyled = styled.input`
+  display: inline-block;
+  width: 300px;
+  height: 30px;
+`;
+
+export const TodoItem: React.FC<TodoItemProps> = (props: TodoItemProps) => {
+  const { id, task, onChange, onBlur } = props;
+  return (
+    <TodoItemStyled
+      id={id}
+      value={task}
+      onChange={onChange}
+      onBlur={onBlur}
+    >
+    </TodoItemStyled>
+  );
+};
